0cd291cf5f588e8c0d2c328ce4decab3b33dce44
rlaclgh <chihokim@innogrid.com>
2024-09-11 14:32:58 +0900

    ADD client layout

---
+++ client/src/app/description.tsx
@@ -0,0 +1,14 @@
+import styles from "./description.module.scss";
+
+const Decription = () => {
+  return (
+    <div className={styles.wrapper}>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate hic
+      iusto enim eaque doloribus saepe repudiandae fugiat totam, iste eos in
+      facilis, quisquam inventore praesentium molestiae quos, voluptas nostrum
+      corporis!
+    </div>
+  );
+};
+
+export default Decription;
