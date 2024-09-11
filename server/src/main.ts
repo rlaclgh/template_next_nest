4a67cfc1ec5708bff49f410a6cb128c455aac1bb
rlaclgh <chihokim@innogrid.com>
2024-09-11 14:37:02 +0900

    [server] ADD swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('miniproject')
+    .setDescription('miniproject')
+    .setVersion('1.0.0')
+    .addTag('miniproject')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(5000);
 }
 bootstrap();
